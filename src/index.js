8e32a84d1f514f8256916f73224ced83507d7cc8
Numel <melvyn.phillips@googlemail.com>
2020-04-30 22:28:14 +0100

    JS Test
    cool that this is able

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Javascript bootcamp</h1>
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+var variable = "Hellojkjkjkj there";
+
+console.log(variable);
