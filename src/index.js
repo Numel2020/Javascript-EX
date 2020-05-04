b2ca84e1c48928c1611e10d3bb1d22e8e0a47a61
Numel <melvyn.phillips@googlemail.com>
2020-05-04 02:52:23 +0100

    last

--- src/index.js
+++ src/index.js
@@ -16,3 +16,20 @@ document.getElementById("app").innerHTML = `
 `;
 
 let log = console.log;
+
+let cardAce = {
+  name: "Ace of Spades"
+};
+
+let cardKing = {
+  name: "King of Spades"
+};
+
+let deck = new Map();
+deck.set("as", cardAce);
+deck.set("kc", cardKing);
+//log(deck.keys());
+
+for (let keys of deck.values()) {
+  log(keys.name);
+}
