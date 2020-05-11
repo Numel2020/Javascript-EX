c5c493889f22fbf384c940cfd69b1d0d4d1ae22c
Numel <melvyn.phillips@googlemail.com>
2020-05-11 22:33:44 +0100

    Javascript fundamentals

--- src/index.js
+++ src/index.js
@@ -1,52 +1,16 @@
 import "./styles.css";
-//import {calc} from "./part1";
+import { calc } from "./test";
 
 document.getElementById("app").innerHTML = `
 <header><h1>Javascript bootcamp</h1></header>
-<nav><button id="btnGo">Go Fetch Something</button></nav>
-<main>
-<div class="box">Box 1</div>
-<div class="box">Box 2</div>
-<div id="spinner" data-state="hide"></div>
 
-</main>
 
-</div>
-`;
 
-let log = console.log;
+</div>`;
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   log("cool");
-//   document.getElementById("btnGo").addEventListener("click", doFetch);
-// });
+//alert('Hello');
+//alert('World');
 
-document.addEventListener("DOMContentLoaded", event => {
-  log("DOM fully loaded and parsed");
-});
-
-// function doFetch(ev) {
-//   log("cool");
-//   ev.preventDefault();
-//   loadSpinnerState('show');
-
-//   fetch("https://reqres.in/api/users/")
-// .then((resp) => resp.json())
-// .then(showContent)
-// .catch(fail)
-//.finally(hideLoading);
-//}
-
-// function loadSpinnerState(prop) {
-//   let spinner = document.getElementById("spinner");
-//   spinner.setAttribute("data-state", `${prop}`);
-// }
-
-// function showContent(data) {
-//   console.log("we got the content");
-//   console.log(data);
-// }
-// function fail(err) {
-//   console.warn("Something went wrong with the fetch");
-//   console.warn(err.message);
-// }
+// alert(3 +
+//   1
+//   + 2);
---
+++ src/test.js
@@ -0,0 +1,54 @@
+//[1, 2].forEach(alert);
+//alert("There will be an error");
+
+//[1, 2].forEach(alert)
+
+// thi isFinit
+// emulti line
+
+//let $ = 1; // declared a variable with the name "$"
+//let _ = 2; // and now a variable with the name "_"
+
+//alert($ + _); // 3
+
+let name = "john";
+let admin = name;
+
+//alert(admin)
+
+//const PLANET = 'earth';
+
+//let currUser = name;
+// only using back ticks
+let you = `the result is ${name}`;
+
+console.log(typeof you);
+
+// TYPEOF OPERATOR
+/* typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function"  (3) */
+
+/* let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed */
+
+//alert, prompt, confirm
