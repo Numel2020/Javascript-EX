17ef86780d799d14b10f2ff9bbba7ad7bc741235
Numel <melvyn.phillips@googlemail.com>
2020-05-18 19:50:50 +0100

    object change

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ document.getElementById("app").innerHTML = `
 </div>`;
 
 const region = [
+  "Africa/Sudan",
   "Africa/Abidjan",
   "Africa/Accra",
   "Africa/Algiers",
@@ -24,27 +25,51 @@ const region = [
   "Africa/Maputo"
 ];
 
+let cities = [];
+let location = [];
+let free = [];
 
-let cities = []
-let location = []
+region.forEach(city => {
+  let dash = city.lastIndexOf("/");
+  let result = city.substring(dash + 1);
 
-
-region.forEach(city=> {
-  let dash = city.lastIndexOf('/');
-  let result = city.substring(dash + 1)
-
-  return cities.push(result)
+  return cities.push(result);
 });
 
 cities.forEach((item, index) => {
   location.push({
     city: item,
     region: region[index]
-  })
-})
-
+  });
+});
 
 console.log(location);
 
+Object.entries(location).forEach((item, index) => {
+  console.log(item[1].city, "", index);
+  free.push({
+    id: index,
+    city: item[1].city,
+    region: item[1].region
+  });
+});
 
+console.log(free);
 
+let ordered = [];
+
+Object.entries(free)
+  .sort((a, b) => {
+    //console.log(a[1].city,b[1].city)
+    let city1 = a[1].city;
+    let city2 = b[1].city;
+    if (city1 > city2) return 1;
+    else if (city2 > city1) return -1;
+    else return 0;
+  })
+  .forEach(item => {
+    console.log(item[1]);
+    ordered.push(item[1]);
+  });
+
+console.log(ordered);
